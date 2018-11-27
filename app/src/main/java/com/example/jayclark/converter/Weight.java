750897269fccdf8f1e347e52dc9575d0701d033e
Jonathan <jclark81@student.gsu.edu>
2018-11-26 20:48:59 -0500

    Latest with Weight.java updated - validation and pop-up, calculation correction in TextView, and calculation rounding cleaned

--- app/src/main/java/com/example/jayclark/converter/Weight.java
+++ app/src/main/java/com/example/jayclark/converter/Weight.java
@@ -1,12 +1,16 @@
 package com.example.jayclark.converter;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -14,34 +18,31 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Weight extends AppCompatActivity {
-    TextView titleView;
+    EditText titleView;
     EditText editWeight;
     TextView resultWeight;
     Button buttonWeight;
     Spinner spinnerWeight;
+    String record;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainfour);
 
-        titleView = (TextView) findViewById(R.id.title_view);
+        titleView = (EditText) findViewById(R.id.title_view);
         editWeight = (EditText) findViewById(R.id.edit_weight);
         resultWeight = (TextView) findViewById(R.id.view_weight);
         buttonWeight = (Button) findViewById(R.id.button_weight);
-        spinnerWeight = (Spinner) findViewById(R.id.spinner_weight);
 
-        String name= titleView.getText().toString();
 
-        if(spinnerWeight != null && spinnerWeight.getSelectedItem() !=null ) {
-            name = (String)spinnerWeight.getSelectedItem();
-            titleView.setText(name);
-        }
+
+
 
         buttonWeight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showCalculation(editWeight, resultWeight);
+                showCalculation(editWeight, resultWeight,titleView.getText().toString());
             }
         });
     }
@@ -51,24 +52,54 @@ public class Weight extends AppCompatActivity {
         mgr.showSoftInput(editWeight, InputMethodManager.SHOW_IMPLICIT);
     }
 
-    public void spinnerChoice(Spinner s, EditText e) {
-        String name= e.getText().toString();
-        if(s != null && s.getSelectedItem() !=null ) {
-            name = (String)s.getSelectedItem();
-            e.setText(name);
+
+
+    public void showCalculation (EditText a, TextView b, String e){
+
+        if(e.equals("Pound")  ) {
+
+            String value = a.getText().toString();
+
+            int finalValue = Integer.parseInt(value);
+
+            double total = finalValue * 0.454;
+
+            String finalResult = Double.toString(total);
+
+            b.setText( finalResult + " Kilogram(s) in " + a.getText().toString() + " Pound(s)");
+
+
+        }else if(e.equals("Kilogram")){
+            String value = a.getText().toString();
+
+            int finalValue = Integer.parseInt(value);
+
+            double total = finalValue * 2.205;
+
+            String finalResult = Double.toString(total);
+
+            b.setText(finalResult);
+
+            b.setText( finalResult + " Pound(s) in " + a.getText().toString() + " Kilogram(s)");
+            } else  {
+            alertBox();
         }
+
+
     }
-    public void showCalculation (EditText a, TextView b){
-        String value = a.getText().toString();
+    public void alertBox() {
+        AlertDialog alertDialog = new AlertDialog.Builder(Weight.this).create();
+        alertDialog.setTitle("Alert");
 
-        int finalValue = Integer.parseInt(value);
+        alertDialog.setMessage("Sorry, the words in the first text field must be 'Pounds' or 'Kilogram'");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
 
-        double total = finalValue * 0.454;
 
-        String finalResult = Double.toString(total);
-
-        b.setText(finalResult);
-
-
+        alertDialog.show();
     }
 }
